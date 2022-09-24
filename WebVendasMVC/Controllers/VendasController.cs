1fe3814babadfb45f996b9f274d83e6fb012c8db
Patrício Amorim <patricius.ti@gmail.com>
2022-09-24 20:16:32 -0300

    Preparando a navegação de telas de pesquisa de vendas

---
+++ WebVendasMVC/Controllers/VendasController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebVendasMVC.Controllers
+{
+    public class VendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaSimples()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaAgrupada()
+        {
+            return View();
+        }
+
+    }
+}
